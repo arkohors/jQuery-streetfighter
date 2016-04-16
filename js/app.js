9dd8434631093cc596bbe71ea42c9a980796b06c
arkohors <arkohors@gmail.com>
2016-04-16 12:33:07 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	ryuanimations();
+});
+
+function ryuanimations(){
+	$('.ryu').mouseenter(function(){
+	$('.ryu-still').hide();
+	$('.ryu-ready').show();
+	})
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function(){
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+		);
+		
+	})
+	.mouseup(function(){
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	})
+	//When User holds down "X" show Ryu-cool pose
+   $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+    }   
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
+};
+
+//play hadouken sound
+//show hadouken and animate it
+function playHadouken(){
+$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+};
+
+
